e64fc30694f4064f9fd03818db69724385449d70
it21102196 <87558697+it21102196@users.noreply.github.com>
2021-07-17 06:22:16 +0000

    Completed the tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
-  printf("Just checking");
+  int mark1,mark2,sum=0;
+  float avg;
+
+  printf("Enter the mark1 and mark 2 : ");
+  scanf("%d %d",&mark1,&mark2);
+  
+  sum = mark1+mark2;
+  avg = sum/2.0;
+  printf("Sum is %d\n",sum);
+  printf("Average is %.2f\n",avg);
   return 0;
 }
 
